b9380a87d89ccc5f166c84173bd2a477fd709755
Seb James <sebsta@gmail.com>
2015-08-23 14:15:43 +0100

    rem

--- gallery/routers.py
+++
@@ -1,6 +0,0 @@
-from gallery.views import GalleryViewSet, PhotoViewSet
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register(r'galleries', GalleryViewSet)
-router.register(r'photos', PhotoViewSet)router.register(r'photos', PhotoViewSet)
